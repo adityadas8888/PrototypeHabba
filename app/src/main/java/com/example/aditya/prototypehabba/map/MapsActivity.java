31b734e25eb0f468b11c2a488de3a2d0ddf7a86b
adityadas8888 <aditya.das8.ad@gmail.com>
2017-02-02 03:21:55 +0530

    3:21am changes

--- app/src/main/java/com/example/aditya/prototypehabba/map/MapsActivity.java
+++ app/src/main/java/com/example/aditya/prototypehabba/map/MapsActivity.java
@@ -64,7 +64,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onBackPressed() {
         super.onBackPressed();
         Intent intent = new Intent(this, MainActivity.class);
-
-    }
+        finish();    }
 }
 
