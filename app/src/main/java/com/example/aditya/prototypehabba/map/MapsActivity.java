1c2a3921361ddd518b8fe8ccf81db42d6107601a
adityadas8888 <aditya.das8.ad@gmail.com>
2017-02-02 02:03:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aditya/prototypehabba/map/MapsActivity.java
@@ -0,0 +1,61 @@
+package com.example.aditya.prototypehabba.map;
+
+import android.content.res.Resources;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.aditya.prototypehabba.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private static final String TAG = MapsActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        try {
+            boolean success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.style_json));
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+        LatLngBounds acharya = new LatLngBounds(new LatLng(13.0849866, 77.4850360), new LatLng(13.1849866, 77.8));
+// Constrain the camera target to the Acharya bounds.
+        mMap.setLatLngBoundsForCameraTarget(acharya);
+        // Add a marker in Sydney and move the camera
+        LatLng cse = new LatLng(13.0849866, 77.4850360);
+
+        mMap.addMarker(new MarkerOptions().position(cse).title("CSE")/*.icon(BitmapDescriptorFactory.fromResource(R.drawable.blue))*/);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(cse)
+                .zoom(20)
+                .bearing(90)
+                .tilt(45)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+        mMap.setBuildingsEnabled(true);
+    }
+    }
+}
